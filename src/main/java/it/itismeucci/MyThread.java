a044b6c06f8d72e97986a24010c19479284c21d1
Gabriel <gabrielsiano2006@gmail.com>
2024-10-14 10:44:52 +0200

    add menu

--- src/main/java/it/itismeucci/MyThread.java
+++ src/main/java/it/itismeucci/MyThread.java
@@ -19,13 +19,36 @@ public class MyThread  extends Thread {
             BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
             DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
             String stringaRicevuta = "";
-
-            do {
+            
+            do {         
+                
                 stringaRicevuta = in.readLine();
+                String op = in.readLine();
+                
+                switch (op) {
+                case "1":
+                stringaRicevuta = stringaRicevuta.toUpperCase();
+                    break;
+
+                case "2":
+                stringaRicevuta = stringaRicevuta.toLowerCase();
+                    break;
+
+                case "3":
+                stringaRicevuta = reverseString(stringaRicevuta);
+                    break; 
+
+                case "4":
+                stringaRicevuta = "Numero caratteri: " + stringaRicevuta.length();
+                    break;
+            
+                default:
+                    break;
+            }
+
                 System.out.println("La stringa ricevuta: " + stringaRicevuta);
 
-                String stringaMaiuscola = stringaRicevuta.toUpperCase();
-                out.writeBytes(stringaMaiuscola + '\n');
+                out.writeBytes(stringaRicevuta + '\n');
 
             } while (!stringaRicevuta.equals("!"));
 
@@ -34,4 +57,13 @@ public class MyThread  extends Thread {
             System.out.println("Errore");
         }
     }
+
+    public String reverseString(String str) {
+        var newString = "";
+        for (var i = str.length() - 1; i >= 0; i--) {
+            newString += str.charAt(i);
+        }
+        return newString;
+    }
 }
+
